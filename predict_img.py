5a892583c0893f495edecdfa41afdf8550c5a7b3
Zhenning Yang <zhenningyang0139@gmail.com>
2021-04-19 20:18:13 -0400

    ...

--- predict_img.py
+++ predict_img.py
@@ -18,12 +18,12 @@ from mit_semseg.utils import colorEncode
 from timeit import default_timer as timer
 
 # pass in mode config(yaml file)
-# return a dict for the file 
+# return a dict for the file
 # return decoder and encoder weights path
 def parse_model_config(path):
     with open(path) as file:
         data = yaml.load(file, Loader=yaml.FullLoader)
-    
+
     encoder_path = None
     decoder_path = None
 
@@ -37,7 +37,7 @@ def parse_model_config(path):
 
     if encoder_path==None or decoder_path==None:
         raise("model weights not found")
-        
+
     return data, encoder_path, decoder_path
 
 def visualize_result(img, pred, index=None):
@@ -45,7 +45,7 @@ def visualize_result(img, pred, index=None):
     if index is not None:
         pred = pred.copy()
         pred[pred != index] = -1
-        print(f'{names[index+1]}:')
+        # print(f'{names[index+1]}:')
 
     # colorize prediction
     pred_color = colorEncode(pred, colors).astype(numpy.uint8)
@@ -91,7 +91,7 @@ def predict_img(segmentation_module, singleton_batch, output_size):
     return pred
 
 
-def get_color_palette(pred, bar_height):
+def get_color_palette(pred, names, colors, bar_height):
 
     pred = np.int32(pred)
     pixs = pred.size
@@ -110,12 +110,12 @@ def get_color_palette(pred, bar_height):
         ratio = counts[idx] / pixs * 100
         if ratio > 0.1:
             print("{}  {}: {:.2f}% {}".format(color_index+1, name, ratio, colors[color_index]))
-            img = cv2.rectangle(img, (0,top_left_y), (250,bottom_right_y), 
+            img = cv2.rectangle(img, (0,top_left_y), (250,bottom_right_y),
                        (int(colors[color_index][0]),int(colors[color_index][1]),int(colors[color_index][2])), -1)
             img = cv2.putText(img, "{}: {:.3f}%".format(name, ratio), (0,top_left_y+20), 5, 1, (255,255,255), 2, cv2.LINE_AA)
             top_left_y+=30
             bottom_right_y+=30
-            
+
     return img
 
 
@@ -157,11 +157,11 @@ def load_model_from_cfg(cfg):
         num_class = model_config['DATASET']['num_class'],
         weights = decoder_path,
         use_softmax=True)
-    
+
     crit = torch.nn.NLLLoss(ignore_index=-1)
     segmentation_module = SegmentationModule(net_encoder, net_decoder, crit)
     return segmentation_module
-    
+
 
 if __name__ == '__main__':
 
@@ -170,16 +170,16 @@ if __name__ == '__main__':
     parser.add_argument("-a", "--alpha", default=0.6, type=float, metavar='', help="transparent overlay level")
     parser.add_argument("-s", "--save", default="tmp_results/", type=str, metavar='', help="save prediction to")
     parser.add_argument("-d", "--display", default=1, type=int, metavar='', help="display real time prediction")
-    
-    parser.add_argument("--cfg", default="config/ade20k-resnet50dilated-ppm_deepsup.yaml", 
+
+    parser.add_argument("--cfg", default="config/ade20k-resnet50dilated-ppm_deepsup.yaml",
                         metavar="FILE", help="path to config file", type=str,)
     parser.add_argument("--gpu", default=0, type=int, metavar='', help="gpu id for evaluation")
-    parser.add_argument("opts", help="Modify config options using the command-line", 
+    parser.add_argument("opts", help="Modify config options using the command-line",
                         default=None, nargs=argparse.REMAINDER, metavar='')
     args = parser.parse_args()
-        
+
     # print(args.save)
-    
+
     # colors
     colors = scipy.io.loadmat('data/color150.mat')['colors']
     names = {}
@@ -188,53 +188,53 @@ if __name__ == '__main__':
         next(reader)
         for row in reader:
             names[int(row[0])] = row[5].split(";")[0]
-    
-    
+
+
     # Network Builders
     print("parsing {}".format(args.cfg))
     start = timer()
     segmentation_module = load_model_from_cfg(args.cfg)
     end = timer()
     segmentation_module.eval()
-    
+
     if torch.cuda.is_available():
         segmentation_module.cuda()
-    
+
     print("Load time: {}".format(end - start))
-    
+
     # predict
     img_original, singleton_batch, output_size = process_img(args.img)
-    
+
     start = timer()
     pred = predict_img(segmentation_module, singleton_batch, output_size)
     end = timer()
     print("Inference time: {}\n".format(end - start))
-    
+
     # print(type(img_original))
     pred_color, org_pred_split = visualize_result(img_original, pred)
-    
+
     # color_palette
-    color_palette = get_color_palette(pred, org_pred_split.shape[0])
-    
+    color_palette = get_color_palette(pred, names, colors, org_pred_split.shape[0])
+
     # transparent pred on org
     dst = transparent_overlays(img_original, pred_color, alpha=args.alpha)
-    
+
     # colored_pred + color_palette
     pred_color_palette = numpy.concatenate((color_palette, pred_color), axis=1)
-    
+
     # transparent pred on org + color_palette
     pred_color_palette_dst = numpy.concatenate((color_palette, dst), axis=1)
-    
+
     # org + colored_pred + color_palette
     pred_color_palette_all = numpy.concatenate((org_pred_split, color_palette), axis=1)
-    
+
     cv2.imwrite("{}/pred_color.png".format(args.save), cv2.cvtColor(pred_color, cv2.COLOR_RGB2BGR))
     cv2.imwrite("{}/org_pred_split.png".format(args.save), cv2.cvtColor(org_pred_split, cv2.COLOR_RGB2BGR))
     cv2.imwrite("{}/dst.png".format(args.save), cv2.cvtColor(dst, cv2.COLOR_RGB2BGR))
     cv2.imwrite("{}/pred_color_palette.png".format(args.save), cv2.cvtColor(pred_color_palette, cv2.COLOR_RGB2BGR))
     cv2.imwrite("{}/pred_color_palette_dst.png".format(args.save), cv2.cvtColor(pred_color_palette_dst, cv2.COLOR_RGB2BGR))
     cv2.imwrite("{}/pred_color_palette_all.png".format(args.save), cv2.cvtColor(pred_color_palette_all, cv2.COLOR_RGB2BGR))
-    
+
     if (args.display)==1:
         PIL.Image.fromarray(pred_color_palette_dst).show()
     else:
